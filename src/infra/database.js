26b6779de4a0e639dbeb5dd921603b6c7ce62f7a
Fernando Rovaris <fernando_rovaris@outlook.com>
2023-05-27 00:15:35 -0300

    crach

--- src/infra/database.js
+++ src/infra/database.js
@@ -1,7 +1,7 @@
 require('dotenv/config');
 const pgp = require("pg-promise")();
 
-const db = pgp ("postgres://doarse_user:fUXsU9DmpBfgbFSuFL6ut0YkY8t6vC0T@dpg-chjbmfndvk4opidf5pgg-a.ohio-postgres.render.com/doarse");
+const db = pgp (process.env.DATABASE_URL);
 
 module.exports = db;
 
