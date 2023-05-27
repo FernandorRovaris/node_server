11f659c1a8125455a7edd92005a8dc9113e2c575
Fernando Rovaris <fernando_rovaris@outlook.com>
2023-05-27 02:27:48 -0300

    a

--- src/service/estadoServices.js
+++ src/service/estadoServices.js
@@ -8,28 +8,19 @@ exports.findAll = async function () {
     var result = await estadoRepositorie.findAll();
 
     var id
-   // var estado = Estados;
-
     a = result.map(function(obj) {
-        var list = [];
         
-
         if (id != obj.id_est){
             estado = new Estados(obj.id_est, obj.nome_est);
             id = obj.id_est;
             return estado;
         }
-
         cidade = new Cidade(obj.id_cit, obj.nome_cit);
         estado.addCid(cidade);
-
-       
         
     }).filter(function(lista) {
         return lista != null;
-      });
-
-    
+    });
 
     console.log(a);
       return a;
