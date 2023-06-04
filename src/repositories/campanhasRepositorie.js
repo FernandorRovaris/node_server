8ce0569fecdfe7c4960a281fb118a52fca96c238
Fernando Rovaris <fernando_rovaris@outlook.com>
2023-06-04 15:13:07 -0300

    campanha

--- src/repositories/campanhasRepositorie.js
+++ src/repositories/campanhasRepositorie.js
@@ -2,7 +2,7 @@ const database = require("../infra/database");
 
 
 exports.insert = function (campanha) {
-    console.log(user);
+    console.log(campanha);
     return database.oneOrNone("insert into doarse.campanhas(users_id, titulo, categoria, item_desc, item_meta, descricao) values ($1, $2, $3, $4, $5, $6) returning *",
                         [campanha.users_id, campanha.titulo, campanha.categoria, campanha.item_desc, campanha_item_meta, campanha.descricao]); 
 
