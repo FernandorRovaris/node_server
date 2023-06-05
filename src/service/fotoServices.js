f6d978c23abe0bcf06b8d9c71b87b88c981fba40
Fernando Rovaris <fernando_rovaris@outlook.com>
2023-06-04 21:09:32 -0300

    help

--- src/service/fotoServices.js
+++ src/service/fotoServices.js
@@ -4,6 +4,8 @@ const Foto = require("../models/fotosCampanhas");
 exports.insert = async function (id, fotos) {
 
     for (const foto in fotos) {
+        console.log("aqui");
+        console.log(foto);
         await fotoRepositorie.insert(id, foto);
     }    
     
