eb9d4fcd94cd5cf2f030c6f99e20d68f80fa1481
Fernando Rovaris <fernando_rovaris@outlook.com>
2023-06-04 21:22:55 -0300

    help

--- src/repositories/fotosRepositorie.js
+++ src/repositories/fotosRepositorie.js
@@ -2,7 +2,7 @@ const database = require("../infra/database");
 
 exports.insert = function (id, foto) {
       
-    return database.oneOrNone("insert into doarse.fotos(campanha_id, foto)  VALUES ($1, $2) returning *", [id, foto.foto]); 
+    return database.oneOrNone("insert into doarse.fotos(campanha_id, foto)  VALUES ($1, $2) returning *", [id, foto]); 
 
 }
 
